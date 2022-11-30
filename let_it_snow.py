8bd8b1da7e672b92156951d1b4e2c0d4f3a47142
Sherin KS <sherin@Sherins-Air.fritz.box>
2022-11-30 12:00:33 +0100

    New feature: random color for each snow flake

--- let_it_snow.py
+++ let_it_snow.py
@@ -14,7 +14,7 @@ def main(speed=0, bg_color="grey"):
   
  
     """TODO: define different colors here"""
-
+    colors=['lightcoral', 'yellowgreen', 'turquoise', 'plum', 'white']
 
     for _ in range(10):
         # define some params
@@ -22,6 +22,8 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
+        color = np.random.choice(colors)
+        myTurtle.color(color)
 
         # Go to the start position of the snowflake
         myTurtle.penup()
